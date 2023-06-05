76d9c21fdd1d929d15240bde52deea08960e8c70
chir263 <31371125+chir263@users.noreply.github.com>
2023-06-05 11:00:47 +0530

    16th May Chirag

---
+++ admin/js/mutation.js
@@ -0,0 +1,18 @@
+let targetNode = document.body;
+var observer = new MutationObserver(function (mutationsList, observer) {
+  let user_name = JSON.parse(localStorage.getItem("netlify-cms-user")).login;
+  var parentDiv = document.querySelector('[class*="AppHeaderContent"]');
+  if (parentDiv) {
+    observer.disconnect();
+    var newDiv = document.createElement("span");
+    newDiv.className = "user_name";
+    newDiv.innerHTML = `${user_name}`;
+    var navTag = parentDiv.querySelector("img");
+    if (navTag) {
+      navTag.insertAdjacentElement("beforebegin", newDiv);
+    } else {
+      parentDiv.appendChild(newDiv);
+    }
+  }
+});
+observer.observe(targetNode, { childList: true, subtree: true });
