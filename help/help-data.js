54ad62c0630213651a6456af7f662a47f6a82a22
chir263 <31371125+chir263@users.noreply.github.com>
2023-06-10 22:36:52 +0530

    16th May Chirag

--- help/help-data.js
+++ help/help-data.js
@@ -1,7 +1,7 @@
 const HELP_DATA = [
   { file: "./markdown/introduction.md", caption: "Introduction" },
   { file: "./markdown/getting-started.md", caption: "Getting Started" },
-  { file: "./markdown/authentication.md", caption: "Authentication" },
+  { file: "./markdown/upload-media.md", caption: "Upload Media" },
   { file: "./markdown/collaborator-access.md", caption: "Collaborator Access" },
   { file: "./markdown/experiment.md", caption: "Experiment Editing" },
 ];
