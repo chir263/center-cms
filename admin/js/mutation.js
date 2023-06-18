2a3653f2eb888bde4fa727d54620c5ef0dd0cd0e
chir263 <31371125+chir263@users.noreply.github.com>
2023-06-18 12:39:00 +0530

    16th May Chirag

--- admin/js/mutation.js
+++ admin/js/mutation.js
@@ -15,20 +15,6 @@ var observer = new MutationObserver(function (mutationsList, observer) {
       parentDiv.appendChild(newDiv);
     }
   }
-
-  // let simulation = [
-  //   "simulation-upload",
-  //   "simulation-upload-css",
-  //   "simulation-upload-js",
-  //   "simulation-upload-images",
-  // ];
-
-  // for (let name of simulation) {
-  //   if (window.location.href.endsWith(name)) {
-  //     element = document.querySelector('[class*="Pane vertical Pane1  "]');
-  //     element.style.width = "100%";
-  //   }
-  // }
 });
 observer.observe(targetNode, { childList: true, subtree: true });
 
@@ -51,7 +37,8 @@ var observer1 = new MutationObserver(function (mutationsList, observer) {
       if (element && button) {
         num++;
         element.style.width = "100%";
-        // button.click();
+        header.style.zIndex = 100000000;
+        button.click();
       }
     }
   }
