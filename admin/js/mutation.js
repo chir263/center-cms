897379ad708301b30f275a4a97dea1d42b403f3c
chir263 <31371125+chir263@users.noreply.github.com>
2023-06-18 16:47:46 +0530

    16th May Chirag

--- admin/js/mutation.js
+++ admin/js/mutation.js
@@ -64,7 +64,7 @@ var observer1 = new MutationObserver(function (mutationsList, observer) {
     if (window.location.href.endsWith(name)) {
       var ReactModalPortal = document.querySelector("div.ReactModalPortal");
       if (ReactModalPortal) {
-        ReactModalPortal.innerHTML = "";
+        // ReactModalPortal.innerHTML = "";
       }
     }
   }
