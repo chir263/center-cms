67875f0590b8d254a67f5d27e8f046016c06e266
chir263 <31371125+chir263@users.noreply.github.com>
2023-06-18 12:36:47 +0530

    16th May Chirag

--- admin/js/mutation.js
+++ admin/js/mutation.js
@@ -16,47 +16,47 @@ var observer = new MutationObserver(function (mutationsList, observer) {
     }
   }
 
-  let simulation = [
-    "simulation-upload",
-    "simulation-upload-css",
-    "simulation-upload-js",
-    "simulation-upload-images",
-  ];
+  // let simulation = [
+  //   "simulation-upload",
+  //   "simulation-upload-css",
+  //   "simulation-upload-js",
+  //   "simulation-upload-images",
+  // ];
 
-  for (let name of simulation) {
-    if (window.location.href.endsWith(name)) {
-      element = document.querySelector('[class*="Pane vertical Pane1  "]');
-      element.style.width = "100%";
-    }
-  }
+  // for (let name of simulation) {
+  //   if (window.location.href.endsWith(name)) {
+  //     element = document.querySelector('[class*="Pane vertical Pane1  "]');
+  //     element.style.width = "100%";
+  //   }
+  // }
 });
 observer.observe(targetNode, { childList: true, subtree: true });
 
-var observer1 = new MutationObserver(function (mutationsList, observer) {
-  let simulation = [
-    "simulation-upload",
-    "simulation-upload-css",
-    "simulation-upload-js",
-    "simulation-upload-images",
-  ];
-  let num = 0;
-  for (let name of simulation) {
-    if (window.location.href.endsWith(name)) {
-      let element = document.querySelector('[class*="Pane vertical Pane1  "]');
-      let button = document.querySelector(
-        '[class*="FileWidgetButton-button-badge-backgroundBadge-badge"]'
-      );
-      let header = document.querySelector('[class*="ToolbarContainer"]');
-      //
-      if (element && button) {
-        num++;
-        element.style.width = "100%";
-        // button.click();
-      }
-    }
-  }
-  if (num === simulation.length) {
-    observer1.disconnect();
-  }
-});
-observer1.observe(targetNode, { childList: true, subtree: true });
+// var observer1 = new MutationObserver(function (mutationsList, observer) {
+//   let simulation = [
+//     "simulation-upload",
+//     "simulation-upload-css",
+//     "simulation-upload-js",
+//     "simulation-upload-images",
+//   ];
+//   let num = 0;
+//   for (let name of simulation) {
+//     if (window.location.href.endsWith(name)) {
+//       let element = document.querySelector('[class*="Pane vertical Pane1  "]');
+//       let button = document.querySelector(
+//         '[class*="FileWidgetButton-button-badge-backgroundBadge-badge"]'
+//       );
+//       let header = document.querySelector('[class*="ToolbarContainer"]');
+//       //
+//       if (element && button) {
+//         num++;
+//         element.style.width = "100%";
+//         // button.click();
+//       }
+//     }
+//   }
+//   if (num === simulation.length) {
+//     observer1.disconnect();
+//   }
+// });
+// observer1.observe(targetNode, { childList: true, subtree: true });
